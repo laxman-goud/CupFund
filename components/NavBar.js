9cd9da25cbcb9abd3d06a25c9b38dfa75d1e8faa
laxman-goud <codewithlaxman92@gmail.com>
2025-08-17 10:08:07 +0530

    update - navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,25 @@
+import Link from "next/link"
+
+/* eslint-disable @next/next/no-img-element */
 const NavBar = () => {
     return (
         <nav className="bg-gray-900 text-white flex justify-between items-center p-4">
-            <div className="logo font-bold text-lg">GetMeAChai</div>
-            <ul className="flex gap-6">
+            <div className="logo font-bold text-lg flex items-center">
+                <img src="/tea.gif" alt="tea" width={44} />
+                <span>Get Me A Chai</span>
+            </div>
+            {/* <ul className="flex gap-6">
                 <li>Home</li>
                 <li>About</li>
                 <li>Projects</li>
                 <li>Sign Up</li>
                 <li>Login</li>
-            </ul>
+            </ul> */}
+            <div>
+                <Link href={"/login"}>
+                    <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer">Login</button>
+                </Link>
+            </div>
         </nav>
     )
 }
