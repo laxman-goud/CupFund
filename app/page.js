bcdc62636f05af393dd81dcf92038a7e4fb31866
laxman-goud <codewithlaxman92@gmail.com>
2025-11-16 11:04:16 +0530

    docs: add inline comments app/page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
 import HomePage from "@/components/HomePage";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { Suspense } from "react";
 import Loader from "@/components/Loader";
-
 
 export default function Home() {
   return (
     <>
+      {/* Vercel performance monitoring */}
       <SpeedInsights />
+
+      {/* Vercel analytics for user interactions */}
       <Analytics />
+
+      {/* Suspense ensures HomePage loads smoothly with a fallback loader */}
       <Suspense fallback={<Loader />}>
         <HomePage />
       </Suspense>
