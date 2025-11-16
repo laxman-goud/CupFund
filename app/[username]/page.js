6859126569979024fcf74ed45a66811e9adc8e90
laxman-goud <codewithlaxman92@gmail.com>
2025-11-16 11:09:16 +0530

    docs: add inline comments username page

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -4,21 +4,31 @@ import Loader from "@/components/Loader";
 import React from "react";
 import { fetchUser } from "@/action/useractions";
 
+// Dynamic route component for: /username
+// This page loads the donation/payment UI for a specific creator
 const Username = ({ params }) => {
   return (
     <>
-    <Suspense fallback={<Loader/>}>
-      <PaymentPage username={params.username}/>
-    </Suspense>
+      {/* Suspense used to show a loader while PaymentPage fetches user/payment data */}
+      <Suspense fallback={<Loader />}>
+        {/* Pass the dynamic username to PaymentPage */}
+        <PaymentPage username={params.username} />
+      </Suspense>
     </>
   );
 };
 
 export default Username;
 
+// This function dynamically sets the page <title> based on the user visited
 export async function generateMetadata({ params }) {
+  
+  // Fetch user details (server-side) using the username from the URL
   const user = JSON.parse(await fetchUser(params.username));
-  const userTitle = user.username ? `Support ${user.username} | CupFund` : "User Not Found | CupFund"
+
+  const userTitle = user.username
+    ? `Support ${user.username} | CupFund`
+    : "User Not Found | CupFund";
 
   return {
     title: userTitle,
